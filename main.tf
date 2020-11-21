5c145d67d4f9ef0d9c659ce7a2381da3ee631bda
Giordano Scuderi <gscuderi@minervanetworks.com>
2020-11-21 15:47:35 +0100

    Added terraform provider syntax for terraform 0.13

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
-provider "azure" {
-  version = "~> 2.36.0"
-
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      # The "hashicorp" namespace is the new home for the HashiCorp-maintained
+      # provider plugins.
+      #
+      # source is not required for the hashicorp/* namespace as a measure of
+      # backward compatibility for commonly-used providers, but recommended for
+      # explicitness.
+      source  = "hashicorp/azurerm"
+      version = "~> 2.36"
+    }
+  }
 }
 
-provider "random" {
-  version = "2.2"
-}
 
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-resources"
